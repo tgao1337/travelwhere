320f05117ceefa2becf418bc4b19fa0b0ab49507
tgao1337 <gaotommy10@gmail.com>
2018-07-24 17:04:06 -0700

    added the api for eventbrite but for a fixed location now.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const url = "https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=37.7735294&location.longitude=-122.4177857";
+  request.get(url, (err, response, body) => {
+    if(err) { console.log(err); }
+    body = JSON.parse(body);
+    console.log(body);
+  });
   res.render('index', { title: 'TravelWhere' });
 });
 
