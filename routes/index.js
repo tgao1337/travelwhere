d1aa705d25db2e5320fc4aea504344f4a3c8ae21
tgao1337 <gaotommy10@gmail.com>
2018-07-25 17:19:28 -0700

    gets set uri api calls but not quite there. need to also get the localstorage to work

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-if (typeof localStorage === "undefined" || localStorage === null) {
+
   var LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./scratch');
-}
+
 
 // const localStorage = require('localStorage');
 
@@ -14,16 +14,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/result', function(req, res, next) {
-  const laa = localStorage.getItem(testlat);
-  const loo = localStorage.getItem(testlon);
-  const url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=${laa}&location.longitude=${loo}`;
+  // const laa = localStorage.getItem("testlat");
+  // const loo = localStorage.getItem("testlon");
+  const laa = localStorage.getItem("37.7735294");
+  const loo = localStorage.getItem("-122.4177857");
+  const url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=37.7735294&location.longitude=-122.4177857`;
+
+  // const url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=${laa}&location.longitude=${loo}`;
   request.get(url, (err, response, body) => {
     if(err) { console.log(err); }
     body = JSON.parse(body);
-    let evens = body.events[0].name.text;
+    let evens = body.events[2].description;
     // console.log(body);
     // console.log("ashuhudashuasdhudashuasdhhuasasasassi");
-    res.render('result', {evens})
+    res.render('result', {evens: evens});
   });
 });
 
