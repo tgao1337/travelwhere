10c7784f65bde543924c5f0051a5c68c7aec801c
tgao1337 <gaotommy10@gmail.com>
2018-08-07 16:41:24 -0700

    um idk

--- routes/index.js
+++ routes/index.js
@@ -29,44 +29,44 @@ router.post('/', function(req, res, next) {
      let lng = response.json.results[0]["geometry"].location.lng;
     // let latlng [];
     var location = {lat: lat, lng: lng};
-
+    res.render('result', {event: location , title: 'GoWhere'});
     // send to the eventbriteapi!
-    return location;
+    // return location;
   })
-  .then((locationresponse) => {
+  // .then((locationresponse) => {
     // console.log(locationresponse.lat);
     // console.log(locationresponse.lng);
 
-    let laa = locationresponse.lat;
-    let loo = locationresponse.lng;
-    let url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=${laa}&location.longitude=${loo}`;
-    // console.log(url);
-    fetch(url).then((res) => {
-      return res.json();
-      // console.log("retuened json");
-    }).then((text) => {
-      let nametext = text.events[x]["name"].text;
-      let distext = text.events[x]["description"].text;
-      let erl = text.events[x]["url"];
-      let picurl = text.events[x]["logo"].url;
-      let starttime = text.events[x]["start"].local;
-      let endtime = text.events[x]["end"].local;
-      let isfree = text.events[x]["is_free"];
-      let freestring = 'This is not a free event.';
-      if(isfree) {
-        freestring = 'This event is free.';
-      }
-      else {
-        freestring = 'This is not a free event.';
-      }
-
-      var event = {name: nametext, description: distext, eventurl:erl, picurl:picurl, start: starttime, end: endtime, free: freestring};
-      res.render('result', { event: event , title: 'GoWhere' });
-    }).catch((err) => {
-      console.log(err);
-    });
+    // let laa = locationresponse.lat;
+    // let loo = locationresponse.lng;
+    // let url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=${laa}&location.longitude=${loo}`;
+    // // console.log(url);
+    // fetch(url).then((res) => {
+    //   return res.json();
+    //   // console.log("retuened json");
+    // }).then((text) => {
+    //   let nametext = text.events[x]["name"].text;
+    //   let distext = text.events[x]["description"].text;
+    //   let erl = text.events[x]["url"];
+    //   let picurl = text.events[x]["logo"].url;
+    //   let starttime = text.events[x]["start"].local;
+    //   let endtime = text.events[x]["end"].local;
+    //   let isfree = text.events[x]["is_free"];
+    //   let freestring = 'This is not a free event.';
+    //   if(isfree) {
+    //     freestring = 'This event is free.';
+    //   }
+    //   else {
+    //     freestring = 'This is not a free event.';
+    //   }
+    //
+    //   var event = {name: nametext, description: distext, eventurl:erl, picurl:picurl, start: starttime, end: endtime, free: freestring};
+    //   res.render('result', { event: event , title: 'GoWhere' });
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
     // send to the eventbriteapi!
-  })
+
   .catch((err) => {
     console.log(err);
   });
@@ -112,6 +112,44 @@ router.post('/', function(req, res, next) {
 
 });
 router.get('/result', function(req, res, next) {
-  res.render('result', { event: event , title: 'GoWhere' });
+  console.log(req);
+
+  // let laa = locationresponse.lat;
+  // let loo = locationresponse.lng;
+  // let url = `https://www.eventbriteapi.com/v3/events/search/?token=3I5EFIZIDVYYTR4SZ5GD&location.latitude=${laa}&location.longitude=${loo}`;
+  // // console.log(url);
+  // fetch(url).then((res) => {
+  //   return res.json();
+  //   // console.log("retuened json");
+  // }).then((text) => {
+  //   let nametext = text.events[x]["name"].text;
+  //   let distext = text.events[x]["description"].text;
+  //   let erl = text.events[x]["url"];
+  //   let picurl = text.events[x]["logo"].url;
+  //   let starttime = text.events[x]["start"].local;
+  //   let endtime = text.events[x]["end"].local;
+  //   let isfree = text.events[x]["is_free"];
+  //   let freestring = 'This is not a free event.';
+  //   if(isfree) {
+  //     freestring = 'This event is free.';
+  //   }
+  //   else {
+  //     freestring = 'This is not a free event.';
+  //   }
+  //
+  //   var event = {name: nametext, description: distext, eventurl:erl, picurl:picurl, start: starttime, end: endtime, free: freestring};
+  //   res.render('result', { event: event , title: 'GoWhere' });
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
+  // // send to the eventbriteapi!
+  // })
+  // .catch((err) => {
+  // console.log(err);
+  // });
+
+
+
+  res.render('result', { event: req.body , title: 'GoWhere' });
 });
 module.exports = router;
